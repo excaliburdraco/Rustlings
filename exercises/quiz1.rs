68a704f6ef28c3d704363116c5068b19f960cf9d
calibur <278093883@qq.com>
2025-02-10 12:24:40 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -31,9 +31,11 @@ fn verify_test() {
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
+    let price5 = calculate_price_of_apples(30);
 
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+    assert_eq!(30, price5)
 }
